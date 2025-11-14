ec47370f27a20be761a90594019caa4c2738a88d
G-ERICA <ericagf21@gmail.com>
2025-11-14 18:07:52 -0300

    Adicionar arquivos de projeto.

---
+++ MongoCSharp-CRUD/Authors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoCSharp_CRUD
+{
+    public class Authors
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+
+        public void CreateAuthor()
+        { }
+
+        public void ReadAuthor()
+        { }
+
+        public void UpdateAuthor()
+        { }
+
+        public void DeleteAuthor()
+        { }
+
+    }
+}
---
+++ MongoCSharp-CRUD/Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoCSharp_CRUD
+{
+    public class Books
+    {
+        public Books() { }
+        public string Id { get; private set; }
+        public string Title { get; private set; }
+        public string AuthorId { get; private set; }
+        public int Year { get; private set; }
+        
+        public void CreateBook() 
+        { }
+
+        public void ReadBook()
+        { }
+
+        public void UpdateBook()
+        { }
+
+        public void DeleteBook()
+        { }
+
+    }
+}
